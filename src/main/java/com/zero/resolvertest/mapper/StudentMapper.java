bc7416108ae3e1faec46ba5a83603e501224b678
hxy <1392119947@qq.com>
2024-08-12 16:44:39 +0800

    4.0

--- src/main/java/com/zero/resolvertest/mapper/StudentMapper.java
+++ src/main/java/com/zero/resolvertest/mapper/StudentMapper.java
@@ -38,9 +38,9 @@ public interface StudentMapper {
     @Update("update student set name = #{name} where id = #{id}")
     Boolean updateName(Long id,String name);
 
-    @Update("update student set name = #{name} where id=#{id}")
+    @Update("update student set name = #{name} where id<=#{id}")
     Boolean updateBatch(Long id,String name);
 
-    @Update("update student set name = #{name} where id=#{id2}")
+    @Update("update student set name = #{name} where id>=#{id2}")
     Boolean saveByCertain(Long id2,String name);
 }
